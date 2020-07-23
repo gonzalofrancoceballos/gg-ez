1cbdfc8848b8767723f4f2a933468a424380ae4c
Gonzalo Franco Ceballos <gonzalofrancoceballos@gmail.com>
2020-07-23 12:46:47 +0200

    [FEATURE] Pre-process pipelines adapted to MongoDB

--- src/gg_ez/pipelines/pre_process/nodes_pre_process.py
+++ src/gg_ez/pipelines/pre_process/nodes_pre_process.py
@@ -1,93 +1,74 @@
-import pandas as pd
+import numpy as np
 
-import logging
-from typing import Iterable
-
-from gg_ez.pipelines.pre_process.core.league import leagues_dict2df
-from gg_ez.utilities.folder_data import FolderData
+from typing import List, Any
 from gg_ez.utilities.processing import apply_multiprocessing
-from gg_ez.utilities.utils import kv_list_to_dict, dict_to_kv_list
-from gg_ez.pipelines.pre_process.core.fixture import wrapper_fixture_league
-from gg_ez.pipelines.pre_process.core.player_fixture import (
-    process_players_fixture_stats,
-)
+from gg_ez.utilities.dict import flatten_dict
 
 
-def pre_process_leagues(leagues: dict) -> pd.DataFrame:
+def pre_process_leagues(leagues: List[dict]) -> List[dict]:
+    """
+    Pre-processes leagues into flatten distionaries that can rapidly be casted
+    to pd.DataFrame
+
+    :param leagues: raw leagues info
+
+    :return: processed leagues
     """
 
-    :param leagues:
+    flatten_leagues = [flatten_dict(data) for data in leagues]
 
-    :return:
+    return flatten_leagues
+
+
+def pre_process_fixtures(fixtures: List[dict], n_cores: int = 1) -> List[dict]:
+    """
+    Pre-processes fixtures into flatten distionaries that can rapidly be casted
+    to pd.DataFrame
+
+    :param fixtures: raw fixtures info
+    :param n_cores: number of corres in multi-processing
+
+    :return: processed fixtures
     """
 
-    leagues_df = leagues_dict2df(leagues)
-
-    return leagues_df
-
-
-def pre_process_fixtures_leagues(
-    fixture_league: FolderData,
-    existing_processed_fixtures: FolderData,
-    n_cores: int = 1,
-) -> Iterable[pd.DataFrame]:
-    """
-
-    :param fixture_league:
-    :param existing_processed_fixtures:
-    :param n_cores:
-
-    :return:
-    """
-
-    logger = logging.getLogger(__name__)
-
-    fixture_league_data = fixture_league.get_data()
-    leagues = dict_to_kv_list(fixture_league_data)
-
-    existing = list(existing_processed_fixtures.paths_dict.values())
-    existing = [p.name.split("_")[0] for p in existing]
-    to_process = [[k, v] for k, v in leagues if k not in existing]
-
-    logger.info(f"Pre-processing games for {len(to_process)} leagues...")
-
-    processed_fixtures = apply_multiprocessing(
-        wrapper_fixture_league, to_process, n_cores=n_cores
-    )
-    processed_fixtures = kv_list_to_dict(processed_fixtures)
+    processed_fixtures = apply_multiprocessing(flatten_dict, fixtures, n_cores=n_cores)
 
     return processed_fixtures
 
 
-def pre_process_players_fixtures(
-    player_fixture_stats: FolderData,
-    existing_processed_players: FolderData,
-    n_cores: int = 1,
-) -> Iterable[pd.DataFrame]:
+def pre_process_players(players: List[dict], n_cores: int = 1) -> List[dict]:
+    """
+    Pre-processes fixtures into flatten distionaries that can rapidly be casted
+    to pd.DataFrame
+
+    :param players: raw players stats
+    :param n_cores: number of corres in multi-processing
+
+    :return: processed players
     """
 
-    :param player_fixture_stats:
-    :param existing_processed_players:
-    :param n_cores:
+    processed_players = apply_multiprocessing(flatten_dict, players, n_cores=n_cores)
+    for player in processed_players:
+        player["rating"] = to_float(player["rating"])
+    return processed_players
 
-    :return:
+
+def to_float(x: Any) -> float:
+    """
+    Converts a string to float
+
+    :param x: value to convert
+
+    :return: converted value
     """
 
-    logger = logging.getLogger(__name__)
+    if isinstance(x, str):
+        x = x.replace(",", ".")
+    else:
+        return np.nan
 
-    player_fixture_stats = player_fixture_stats.get_data()
-    stats = dict_to_kv_list(player_fixture_stats)
-
-    existing = list(existing_processed_players.paths_dict.values())
-    existing = [p.name.split("_")[0] for p in existing]
-    to_process = [[k, v] for k, v in stats if k not in existing]
-
-    logger.info(f"Pre-processing stats for {len(to_process)} games...")
-
-    processed_stats = apply_multiprocessing(
-        process_players_fixture_stats, to_process, n_cores=n_cores
-    )
-
-    processed_stats = kv_list_to_dict(processed_stats)
-
-    return processed_stats
+    try:
+        x = float(x)
+    except ValueError:
+        x = np.nan
+    return x
