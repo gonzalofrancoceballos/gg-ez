277cae4ef07a75592f24e9759f04bc522b41b150
Gonzalo Franco Ceballos <gonzalofrancoceballos@gmail.com>
2020-07-23 13:41:50 +0200

    [FIX] Filter for leagues that have stats applied in the wrong place

--- src/gg_ez/pipelines/fetch/core/helpers.py
+++ src/gg_ez/pipelines/fetch/core/helpers.py
@@ -4,26 +4,31 @@ from gg_ez.pipelines.pre_process.core.league import leagues_dict2df
 
 
 def get_league_ids(
-    leagues: List[dict], valid_leagues: List[List[str]], only_current: bool = False
+    leagues: List[dict],
+    valid_leagues: List[List[str]],
+    only_current: bool = False,
+    fixtures_players_statistics: bool = False,
 ):
     """
     Filters league ids to download based on is of leagues
 
-    :param valid_leagues:
-    :param leagues:
-    :param only_current:
+    :param valid_leagues: list of valid leagues to consider
+    :param leagues: all leagues info
+    :param only_current: filter leagues taking place in current seasons
+    :param fixtures_players_statistics: filter leagues that have player-fixture stats
 
-    :return:
+    :return: list of filteres leagues
     """
 
     leagues = leagues_dict2df(leagues)
     valid_leagues_df = pd.DataFrame(valid_leagues, columns=["country", "name"])
     leagues = pd.merge(leagues, valid_leagues_df)
     if only_current:
-        leagues = leagues[
-            (leagues["is_current"] == 1)
-            & (leagues["coverage_fixtures_players_statistics"] == True)
-        ].copy()
+        leagues = leagues[(leagues["is_current"] == 1)].copy()
+
+    if fixtures_players_statistics:
+        leagues = leagues[(leagues["coverage_fixtures_players_statistics"] == True)]
+
     league_ids = leagues["league_id"]
     return league_ids
 
